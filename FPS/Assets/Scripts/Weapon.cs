179fafeac1c88a1bb1b22bacf9b74289b0d453c8
Wellbek <louis.wellmeyer@gmail.com>
2020-03-14 15:49:08 +0100

    Shot Impact VFX

--- FPS/Assets/Scripts/Weapon.cs
+++ FPS/Assets/Scripts/Weapon.cs
@@ -9,6 +9,13 @@ public class Weapon : MonoBehaviour
     public Gun[] loadout; //Erst-, Zweit-, Drittwaffe
     public Transform weaponParent;
 
+    //Bulletholes, Particles on Gunshot impact
+    public GameObject bulletholePrefab;
+    public GameObject hitSurvacePrefab;
+    public GameObject hitBloodPrefab;
+    public LayerMask canBeShotSurvace;
+
+
     private int currentIndex;
     private GameObject currentWeapon;
 
@@ -20,7 +27,14 @@ public class Weapon : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
 
-        if (currentWeapon) Aim(Input.GetMouseButton(1));
+        if (currentWeapon)
+        {
+            Aim(Input.GetMouseButton(1));
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }      
     }
 
     #endregion
@@ -55,6 +69,27 @@ public class Weapon : MonoBehaviour
             anchor.position = Vector3.Lerp(anchor.position, state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed); //aus Visir gehen
 
         }
+    }
+
+    private void Shoot()
+    {
+        Transform spawn = transform.Find("Cameras/Normal Camera"); //Raycast aus der Sicht des Spielers, um mittig yzu schiessen.
+                                                                   //Ort aus dem der Spieler schiesst.
+
+        RaycastHit hit = new RaycastHit(); //Objekt, das von dem Raycast getroffen wird.
+        if (Physics.Raycast(spawn.position, spawn.forward, out hit, 1000f, canBeShotSurvace))
+        {
+            GameObject newHole = Instantiate(bulletholePrefab, hit.point + hit.normal * 0.001f, Quaternion.identity) as GameObject; //hit.point, Ort, an dem der Raycast mit dem Objekt interagiert
+                                                                                                                                    //hit.normal * 0.001f => direkt ueber der Layer des Objektes, wir wollen bullethole nicht unter dem Objekt haben.
+            newHole.transform.LookAt(hit.point + hit.normal); //um aus Richtung des interagierten Objekts "zu gucken"
+
+            GameObject newImpactVFX = Instantiate(hitSurvacePrefab, hit.point + hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            newImpactVFX.transform.LookAt(hit.point + hit.normal); //um aus Richtung des interagierten Objekts "zu gucken"
+
+            Destroy(newHole, 5f); //nach 5 sekunden wird das Loch zerstoert
+            Destroy(newImpactVFX, 1f); //nach 5 sekunden wird das Loch zerstoert
+        }
+
     }
 
     #endregion
